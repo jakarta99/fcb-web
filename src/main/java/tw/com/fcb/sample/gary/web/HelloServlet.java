f048a48c4dc60745e57ed527e524934f22173cdd
Gary Lee <jakarta99@gmail.com>
2022-02-18 22:13:05 +0800

    add index page

--- src/main/java/tw/com/fcb/sample/gary/web/HelloServlet.java
+++ src/main/java/tw/com/fcb/sample/gary/web/HelloServlet.java
@@ -3,11 +3,11 @@ package tw.com.fcb.sample.gary.web;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 
 
@@ -32,7 +32,7 @@ public class HelloServlet extends HttpServlet {
 
 		PrintWriter out = response.getWriter();
 		
-		out.println("Hello "+request.getParameter("name"));
+		out.println("Hello id="+request.getParameter("id"));
 		
 		
 		// response.getWriter().append("Served at: ").append(request.getContextPath());
---
+++ webapp/index.jsp
@@ -0,0 +1,4 @@
+Hello, fcb-web
+
+<li> <a href='./hello'>HelloServlet getAll</a> </li>
+<li> <a href='./hello?id=3'>HelloServlet getById with id=3</a> </li>
